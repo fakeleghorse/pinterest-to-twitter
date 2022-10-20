484cc9fef582b8616ebcb2e9c8453cd41fa5537c
randomriffs <fakeleghorse@gmail.com>
2022-10-20 14:04:06 +0530

    fixed resolve method

--- index.js
+++ index.js
@@ -44,69 +44,12 @@ let blogName = 'pappater';
 let params = {}
 const directory = './media/image/'
 
- async function tweetToTumblr (imgUrl) {
-    try {
-        fs.readdir(directory, (err, files) => {
-            if (err) throw err;
-            // removing file 
-            for (const file of files) {
-                fs.unlink(path.join(directory, file), err => {
-                    if (err) throw err;
-                    console.log('file removed')
-                });
-            }
+function tweetToTumblr (imgUrl,) {
+    // try {
+       
+    // } catch (error) {
         
-        // removed file
-        // axios.get(webpageUrl)
-        //     .then(function (response) {
-                // let webPageData = response.data.dataList.data;
-                // let webPageDataSize = webPageData.length;
-                // let randomWebPageIndex = Math.floor(Math.random() * webPageDataSize);
-                // let randomWebPageData = webPageData[randomWebPageIndex]
-                // console.log('webpagedata',randomWebPageData)
-                // let randomWebPageUrl = randomWebPageData.siteUrl;
-                // let imageUrlOptimize = randomWebPageData && randomWebPageData.imageUrl && randomWebPageData.imageUrl.length > 0 && randomWebPageData.imageUrl.filter((eachUrl)=>{
-                //     return eachUrl.includes('optimize')
-                // })
-                // console.log('imageUrl Ptomize', imageUrlOptimize)
-                console.log("imgUrl", imgUrl);
-                const options = {
-                    url: imgUrl,
-                    dest: '../../media/image/current.jpg'                // will be saved to /path/to/dest/image.jpg
-                }
-                console.log("options", options);
-                download.image(options)
-                        .then(({ filename }) => {
-                            console.log("filname", filename);
-                            setTimeout(()=>{
-                                const imageData = fs.readFileSync("media/image/current.jpg") //replace with the path to your image
-                                twtClient.post("media/upload", { media: imageData }, function (error, media, response) {
-                                    if (error) {
-                                        console.log(error)
-                                    } else {
-                                        const status = {
-                                            status: "",
-                                            media_ids: media.media_id_string
-                                        }
-    
-                                        twtClient.post("statuses/update", status, function (error, tweet, response) {
-                                            if (error) {
-                                                console.log(error)
-                                            } else {
-                                                console.log("Successfully tweeted an image!")
-                                            }
-                                        })
-                                    }
-                                })
-                            },5000)
-                        })
-            // }).catch(err => {
-            //     console.log('axios error',err)
-            // });
-    });
-    } catch (error) {
-        
-    }
+    // }
   
 }
 async function doTumblrPhotoPost() {
@@ -114,7 +57,7 @@ async function doTumblrPhotoPost() {
     for (let i = 0, p = Promise.resolve(); i < feed.items.length; i++) {
         p = p.then(_ => new Promise(resolve => {
             let docRef = db.collection("tweeturls").doc("url");
-            docRef.get().then(async (doc) => {
+            docRef.get().then( (doc) => {
                 if (doc.exists) {
                     let existingUrl = doc.data().data;
                     console.log("existingUrl", i);
@@ -123,14 +66,84 @@ async function doTumblrPhotoPost() {
                     params.source = imageUrlFirstLvl;
                     if (!existingUrl.includes(imageUrlFirstLvl)) {
                         console.log("no include");
-                        db.collection("tweeturls").doc("url").set({ data: [...existingUrl, imageUrlFirstLvl] });
+                       
                         // post to tumblr
                         // client.createPhotoPost(blogName, params, function (err, resp) {
                         //     console.log("posted to tumblr >>>>>");
                         //     resolve();
                         // });
-                       await tweetToTumblr(imageUrlFirstLvl)
-                        resolve();
+                        // tweetToTumblr(imageUrlFirstLvl,  resolve())
+                        try {
+                            fs.readdir(directory, (err, files) => {
+                                console.log("files",files);
+                                if (err) throw err;
+                                // removing file 
+                                if(files && files.length>0){
+                                    for (const file of files) {
+                                        fs.unlink(path.join(directory, file), err => {
+                                            if (err) throw err;
+                                            console.log('file removed')
+                                        });
+                                    }
+                                }
+                                
+                            
+                            // removed file
+                            // axios.get(webpageUrl)
+                            //     .then(function (response) {
+                                    // let webPageData = response.data.dataList.data;
+                                    // let webPageDataSize = webPageData.length;
+                                    // let randomWebPageIndex = Math.floor(Math.random() * webPageDataSize);
+                                    // let randomWebPageData = webPageData[randomWebPageIndex]
+                                    // console.log('webpagedata',randomWebPageData)
+                                    // let randomWebPageUrl = randomWebPageData.siteUrl;
+                                    // let imageUrlOptimize = randomWebPageData && randomWebPageData.imageUrl && randomWebPageData.imageUrl.length > 0 && randomWebPageData.imageUrl.filter((eachUrl)=>{
+                                    //     return eachUrl.includes('optimize')
+                                    // })
+                                    // console.log('imageUrl Ptomize', imageUrlOptimize)
+                                    // console.log("imgUrl", imgUrl);
+                                    const options = {
+                                        url: imageUrlFirstLvl,
+                                        dest: '../../media/image/current.jpg'                // will be saved to /path/to/dest/image.jpg
+                                    }
+                                    console.log("options", options);
+                                    download.image(options)
+                                            .then(({ filename }) => {
+                                                console.log("filname", filename);
+                                                setTimeout(()=>{
+                                                    const imageData = fs.readFileSync("media/image/current.jpg") //replace with the path to your image
+                                                    twtClient.post("media/upload", { media: imageData }, function (error, media, response) {
+                                                        if (error) {
+                                                            console.log(error)
+                                                            resolve();
+                                                        } else {
+                                                            const status = {
+                                                                status: "",
+                                                                media_ids: media.media_id_string
+                                                            }
+                                                            twtClient.post("statuses/update", status, function (error, tweet, response) {
+                                                                if (error) {
+                                                                    console.log(error)
+                                                                    resolve();
+                                                                } else {
+                                                                    console.log("Successfully tweeted an image!")
+                                                                    db.collection("tweeturls").doc("url").set({ data: [...existingUrl, imageUrlFirstLvl] });
+                                                                    resolve();
+                                                                }
+                                                            })
+                                                        }
+                                                    })
+                                                },5000)
+                                            })
+                                // }).catch(err => {
+                                //     console.log('axios error',err)
+                                // });
+                        });
+                        } catch (error) {
+                            console.log("errror", error);
+                        }
+                      
+                       
                     }
                 } else {
                     console.log("No such document!");
@@ -145,11 +158,11 @@ async function doTumblrPhotoPost() {
 
 
 };
-// doTumblrPhotoPost();
-// cron.schedule("*/15 * * * *", () => {
-// console.log("calling >>>>>>> doTumblrPhotoPost()");
-// doTumblrPhotoPost();
-// });
+doTumblrPhotoPost();
+cron.schedule("*/15 * * * *", () => {
+console.log("calling >>>>>>> doTumblrPhotoPost()");
+doTumblrPhotoPost();
+});
 
 app.get('/', (req, res) => {
     res.send('hello world')
